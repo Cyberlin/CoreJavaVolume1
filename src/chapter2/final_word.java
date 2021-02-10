e42cb2fa3f4fbb94479675b99b331f3dc8c8778b
Cyberlin <1242546625@qq.com>
2021-02-10 17:54:05 +0800

    test final key word

---
+++ src/chapter2/final_word.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class final_word {
+    public static void main(String[] args) {
+        final  String CM_PER_INCH ="123";
+        double peperWidth = 8.5;
+        double peperLength =15.3;
+        System.out.println(peperLength+CM_PER_INCH+"constant operation");
+
+    }
+}
+
